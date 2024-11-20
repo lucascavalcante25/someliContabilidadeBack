149ecdd9dff8a08258f8df52a72a6ea12a1c3396
Lucas Cavalcante <lucaseuacreditoemdeus@gmail.com>
2024-11-20 00:28:09 -0300

    Commit inicial Back Someli

---
+++ src/main/java/com/someli/someli/SomeliApplication.java
@@ -0,0 +1,13 @@
+package com.someli.someli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomeliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomeliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/someli/someli/SomeliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.someli.someli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomeliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
