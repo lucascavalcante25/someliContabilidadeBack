60cc81b46cd002f76e66ff10dfb3d12c24620159
Lucas Cavalcante <lucaseuacreditoemdeus@gmail.com>
2025-03-10 18:32:45 -0300

    Removendo configuração manual do DataSource para usar as variáveis de ambiente do Render

--- src/main/java/com/someli/someli/DataConfiguration.java
+++ src/main/java/com/someli/someli/DataConfiguration.java
@@ -1,14 +1,35 @@
 package com.someli.someli;
 
+import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @Configuration
 public class DataConfiguration {
 
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.postgresql.Driver");
+		dataSource.setUrl(System.getenv("SPRING_DATASOURCE_URL"));
+		dataSource.setUsername(System.getenv("SPRING_DATASOURCE_USERNAME"));
+		dataSource.setPassword(System.getenv("SPRING_DATASOURCE_PASSWORD"));
+		return dataSource;
+	}
+
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabasePlatform("org.hibernate.dialect.PostgreSQLDialect");
+		adapter.setShowSql(true);
+		adapter.setGenerateDdl(true);
+		return adapter;
+	}
+	
     // Remova este Bean para que o Spring Boot use as variáveis de ambiente configuradas no Render
     /*
     @Bean
@@ -21,13 +42,4 @@ public class DataConfiguration {
         return dataSource;
     }
     */
-
-    @Bean
-    public JpaVendorAdapter jpaVendorAdapter() {
-        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-        adapter.setDatabasePlatform("org.hibernate.dialect.PostgreSQLDialect");
-        adapter.setShowSql(true);
-        adapter.setGenerateDdl(true);
-        return adapter;
-    }
 }
