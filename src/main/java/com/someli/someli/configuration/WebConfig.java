0ca7ca645963388b2885a57533def0153479f2b6
Lucas Cavalcante <lucaseuacreditoemdeus@gmail.com>
2025-03-11 11:15:15 -0300

    Removendo configuração manual do DataSource para usar as variáveis de ambiente do Render

--- src/main/java/com/someli/someli/configuration/WebConfig.java
+++ src/main/java/com/someli/someli/configuration/WebConfig.java
@@ -9,7 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedOrigins("*")
+		registry.addMapping("/**").allowedOrigins("http://localhost:4200")
+				.allowedOrigins("https://someli-contabilidade-web.vercel.app")
 				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedHeaders("*").allowCredentials(true);
 	}
 }
