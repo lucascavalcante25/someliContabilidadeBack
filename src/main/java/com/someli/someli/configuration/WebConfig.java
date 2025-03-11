6e185c4945dc039e3d228ea17e9b408b0214113e
Lucas Cavalcante <lucaseuacreditoemdeus@gmail.com>
2025-03-11 11:06:47 -0300

    Removendo configuração manual do DataSource para usar as variáveis de ambiente do Render

--- src/main/java/com/someli/someli/configuration/WebConfig.java
+++ src/main/java/com/someli/someli/configuration/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("http://localhost:4200") // URL do frontend
+		registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedOrigins("*")
 				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedHeaders("*").allowCredentials(true);
 	}
 }
