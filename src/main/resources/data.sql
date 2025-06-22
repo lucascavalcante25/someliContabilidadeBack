ef4ae497816381f1df0508a9725facb43a517afb
Lucas Cavalcante <lucaseuacreditoemdeus@gmail.com>
2025-06-22 00:50:19 -0300

    ajusta layout e regra da tela de dashboard despesas do escritorio

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.usuario (id, cpf, email, nome, senha, perfil)
+VALUES
+  (1, '06293144384', 'lucasueacreditoemdeus@gmail.com', 'LUCAS CAVALCANTE DE QUEIROZ', '$2a$10$g6LwCXF8z3l7nNtkNHc.aus1PL9ZAZpSiVKmj2b6HUcP7Mz.SdBEu', 1),
+  (2, '09869087698', 'hjkgfvjhg@gmail.com', 'Ana Karolina', '$2a$10$0tCpvJbWaL8qUk0R4T8eKXFBaY/xi3C9MiWwTasH/.0AQk4eO', 1);
