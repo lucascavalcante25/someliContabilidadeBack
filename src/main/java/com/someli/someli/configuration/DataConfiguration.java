b5ae93d1aadaf10aedb08738271916550c20ed2e
Lucas Cavalcante <lucaseuacreditoemdeus@gmail.com>
2025-06-22 23:00:43 -0300

    ajusta layout e regra da tela de dashboard despesas do escritorio

--- src/main/java/com/someli/someli/configuration/DataConfiguration.java
+++ src/main/java/com/someli/someli/configuration/DataConfiguration.java
@@ -11,25 +11,25 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 @Configuration
 public class DataConfiguration {
 
-	@Bean
-	public DataSource dataSource() {
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("org.postgresql.Driver");
-		dataSource.setUrl("jdbc:postgresql://localhost:5432/SomeliApp");
-		dataSource.setUsername("postgres");
-		dataSource.setPassword("postgres");
-		return dataSource;
-	}
+//	@Bean
+//	public DataSource dataSource() {
+//		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//		dataSource.setDriverClassName("org.postgresql.Driver");
+//		dataSource.setUrl("jdbc:postgresql://localhost:5432/SomeliApp");
+//		dataSource.setUsername("postgres");
+//		dataSource.setPassword("postgres");
+//		return dataSource;
+//	}
 
-//	  @Bean
-//	    public DataSource dataSource() {
-//	        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//	        dataSource.setDriverClassName("org.postgresql.Driver");
-//	        dataSource.setUrl(System.getenv("SPRING_DATASOURCE_URL"));
-//	        dataSource.setUsername(System.getenv("SPRING_DATASOURCE_USERNAME"));
-//	        dataSource.setPassword(System.getenv("SPRING_DATASOURCE_PASSWORD"));
-//	        return dataSource;
-//	    }
+	  @Bean
+	    public DataSource dataSource() {
+	        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+	        dataSource.setDriverClassName("org.postgresql.Driver");
+	        dataSource.setUrl(System.getenv("SPRING_DATASOURCE_URL"));
+	        dataSource.setUsername(System.getenv("SPRING_DATASOURCE_USERNAME"));
+	        dataSource.setPassword(System.getenv("SPRING_DATASOURCE_PASSWORD"));
+	        return dataSource;
+	    }
 
 	@Bean
 	public JpaVendorAdapter jpaVendorAdapter() {
