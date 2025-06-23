97d34da2338ba55f5ec6a39f218e0ec168f6a510
Lucas Cavalcante <lucaseuacreditoemdeus@gmail.com>
2025-06-22 22:54:33 -0300

    ajusta layout e regra da tela de dashboard despesas do escritorio

--- src/main/java/com/someli/someli/controller/AuthController.java
+++ src/main/java/com/someli/someli/controller/AuthController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import com.someli.someli.model.Usuario;
 import com.someli.someli.repository.UsuarioRepository;
 import com.someli.someli.util.JwtUtil;
 
+@CrossOrigin(origins = { "http://localhost:4200", "https://someli-contabilidade-web.vercel.app" }, allowCredentials = "true")
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
